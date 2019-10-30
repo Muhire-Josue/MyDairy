a3f821c0b7f2f15b3f66d0216f343941d358463a
Muhire-Josue <rutayisire.josue09@gmail.com>
2019-10-30 11:31:26 +0200

    chore(check for routes): add check for routes
    	- add Check if route exist.
    	- add Check for proper HTTP method for existing route.
    	- add Remove min and max upon creating an entry.
    
    [Finishes #169445814]

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-console */
 import express from 'express';
 import routes from './server/routes/router';
@@ -7,5 +8,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(routes);
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.use((req, res, next) => {
+  if (!req.route) { return res.status(400).json({ status: 400, error: 'Incorrect Route' }); }
+  next();
+});
 
 export default app;
